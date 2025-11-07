2f34fa7dcf758da41dcc97ce8c06d1412ca2454a
unknown <ahmednasser1902@gmail.com>
2025-11-07 05:45:06 +0200

    adding email in registration form

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -3,13 +3,27 @@ from rest_framework import serializers
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
+    email = serializers.EmailField(required=True)
+    first_name = serializers.CharField(required=True) 
+    last_name = serializers.CharField(required=True)  
 
     class Meta:
         model = User
-        fields = ["username", "password"]
+        fields = ["username", "email", "first_name", "last_name", "password"]
+
+    def validate_email(self, value):
+        if User.objects.filter(email=value).exists():
+            raise serializers.ValidationError("Email is already registered.")
+        return value
 
     def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
-        user.is_staff = False # admin flag
+        user = User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data["email"],
+            first_name=validated_data["first_name"],
+            last_name=validated_data["last_name"],
+            password=validated_data["password"]
+        )
+        user.is_staff = False  
         user.save()
         return user
